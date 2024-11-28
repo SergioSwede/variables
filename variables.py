ea243496a2047a1ceb89aff3949a96b56e826301
SergioSwede <s.n.o.r.k.00yandex@gmail.com>
2024-11-28 18:16:41 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,9 @@
+finished_lessons = 12
+spended_hours = 1.5
+course = 'Python'
+time_for_lesson = spended_hours / finished_lessons
+
+print('Курс:', str(course) + ', всего задач:',
+       str(finished_lessons) + ', затрачено часов:',
+       str(spended_hours) + ', среднее время выполнения',
+       str(time_for_lesson), 'часа')
